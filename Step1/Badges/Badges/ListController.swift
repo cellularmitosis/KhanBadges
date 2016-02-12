006922cdf80739f39abff05e29650b3fc6d1ca8d
Jason Pepas <jasonpepas@gmail.com>
2016-02-11 21:58:17 -0600

    Step1: pulling in images into asset library.  pulling in a selection of badge titles.  implementing table view data source.

--- Step1/Badges/Badges/ListController.swift
+++ Step1/Badges/Badges/ListController.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2016 Pepas Labs. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ListController: UITableViewController
+{
+    // this selection chosen to represent all cases of line-length (for the english translations).
+    let titles = [
+        "Hour of Drawing with Code",
+        "Collaborator",
+        "Probability and statistics: Random variables and probability distributions",
+        "Guru",
+        "Precalculus: Probability and combinatorics",
+        "Like Clockwork",
+        "Apprentice Programmer",
+        "Algebra II: Polynomial expressions, equations, and functions",
+        "Tesla",
+        "HTML/CSS: Making webpages",
+    ]
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListCell")!
+
+        let imageName = String(format: "Image-%i", indexPath.row % 19)
+        cell.imageView?.image = UIImage(named: imageName)!
+        
+        cell.textLabel?.text = titles[indexPath.row % titles.count]
+        
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 246
+    }
+}
+
