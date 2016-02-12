585b5efb0ebc2ec2626ce99739f5435d6d8a7f54
Jason Pepas <jasonpepas@gmail.com>
2016-02-11 23:14:56 -0600

    Step1: first pass at grid screen.

--- Step1/Badges/Badges/GridController.swift
+++ Step1/Badges/Badges/GridController.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2016 Pepas Labs. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class GridCell: UICollectionViewCell
+{
+    @IBOutlet weak var imageView: UIImageView!
+}
+
+class GridController: UICollectionViewController
+{
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("GridCell", forIndexPath: indexPath) as! GridCell
+        
+        let imageName = String(format: "Image-%i", indexPath.row % 19)
+        cell.imageView?.image = UIImage(named: imageName)!
+        
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 246
+    }
+}
