801157af612465d0c811255030850854b8118593
Jason Pepas <jasonpepas@gmail.com>
2016-02-11 20:54:03 -0600

    skeleton for Step1 app

---
+++ Step1/Badges/Badges/GridController.swift
@@ -0,0 +1,9 @@
+//
+//  GridController.swift
+//  Badges
+//
+//  Created by Pepas Personal on 2/11/16.
+//  Copyright © 2016 Pepas Labs. All rights reserved.
+//
+
+import Foundation
---
+++ Step1/Badges/Badges/ListController.swift
@@ -0,0 +1,9 @@
+//
+//  ListController.swift
+//  Badges
+//
+//  Created by Pepas Personal on 2/11/16.
+//  Copyright © 2016 Pepas Labs. All rights reserved.
+//
+
+import Foundation
