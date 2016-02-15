d639655bf1f9335873300d359a7740cfe337fb18
Jason Pepas <jasonpepas@gmail.com>
2016-02-15 16:53:26 -0600

    Step3: pulling in Reachability.swift

--- Step3/Badges/Badges/AppDelegate.swift
+++ Step3/Badges/Badges/AppDelegate.swift
@@ -12,46 +12,51 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var reachability: Reachability?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-
+        _setupReachability()
+        
         let url = NSURL(string: "https://www.khanacademy.org/api/v1/badges")!
         let resourceService = ServiceRepository.sharedInstance.resourceServiceForURL(url: url)
         let dataSourceService = BadgeTableViewController.DataSourceService(resourceService: resourceService, serviceRepository: ServiceRepository.sharedInstance)
         let navController = BadgeTableViewController.instantiateInNavigationControllerFromStoryboard(dataSourceService: dataSourceService)
         
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
         
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    private func _setupReachability()
+    {
+        do
+        {
+            reachability = try Reachability.reachabilityForInternetConnection()
+            reachability?.whenReachable = { reachability in
+                dispatch_async(dispatch_get_main_queue(), { [weak self] () -> Void in
+                    guard let weakSelf = self else { return }
+                    weakSelf.applicationDidComeOnline()
+                    })
+            }
+            try reachability?.startNotifier()
+        } catch {}
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func applicationDidComeOnline()
+    {
+        ServiceRepository.sharedInstance.retryFailedRequests()
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication)
     {
         ServiceRepository.sharedInstance.retryFailedRequests()
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    func applicationDidReceiveMemoryWarning(application: UIApplication) {
+    func applicationDidReceiveMemoryWarning(application: UIApplication)
+    {
         ServiceRepository.sharedInstance.didReceiveMemoryWarning()
     }
 }
